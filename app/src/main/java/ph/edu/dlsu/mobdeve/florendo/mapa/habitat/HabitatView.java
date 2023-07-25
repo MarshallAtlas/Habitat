f6a1f37a567ff201b6d4c82920461ef4daa9c3dc
Jamie <57598321+jamieeow@users.noreply.github.com>
2023-07-25 20:07:53 +0800

    first

---
+++ app/src/main/java/ph/edu/dlsu/mobdeve/florendo/mapa/habitat/HabitatView.java
@@ -0,0 +1,14 @@
+package ph.edu.dlsu.mobdeve.florendo.mapa.habitat;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class HabitatView extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_habitat_view);
+    }
+}
